c4c84a2c73db43f5571875ca0d0586eb9649a6b5
AngelC <noel0402.ac@gmail.com>
2024-07-13 14:16:58 -0500

    Inital commit

---
+++ frontend/main.js
@@ -0,0 +1,8 @@
+counter = document.querySelector("counter");
+async function updateCounter() {
+    let response = await fetch("https://sahnslbz4wpbwuojinuox3w5oi0awcqi.lambda-url.us-east-1.on.aws/")
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`
+}
+
+updateCounter();
