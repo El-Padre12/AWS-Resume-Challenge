ecf58e3c3c28febcd63e5751f9205d066e0375d9
AngelC <noel0402.ac@gmail.com>
2024-07-15 11:00:37 -0500

    working on main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ const counter = document.querySelector("counter");
 async function updateCounter() {
     let response = await fetch("https://sahnslbz4wpbwuojinuox3w5oi0awcqi.lambda-url.us-east-1.on.aws/")
     let data = await response.json();
-    counter.innerHTML = ` ${data}`
+    counter.innerHTML = ` views: ${data}`
 }
 
 updateCounter();
