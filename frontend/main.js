184027c2ea4f349771da9c67b0cb2d456f80e2e7
AngelC <noel0402.ac@gmail.com>
2024-07-15 10:20:27 -0500

    changed counter to const in main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-counter = document.querySelector("counter");
+const counter = document.querySelector("counter");
 async function updateCounter() {
     let response = await fetch("https://sahnslbz4wpbwuojinuox3w5oi0awcqi.lambda-url.us-east-1.on.aws/")
     let data = await response.json();
